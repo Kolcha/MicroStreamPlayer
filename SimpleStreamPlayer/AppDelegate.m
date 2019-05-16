338c1051013fb76eb22064c3fb1eb9946ac47921
Nick Korotysh <kolchaprogrammer@list.ru>
2019-05-16 05:10:28 +0300

    rewritten from Swift

--- SimpleStreamPlayer/AppDelegate.m
+++ SimpleStreamPlayer/AppDelegate.m
@@ -7,21 +7,74 @@
 //
 
 #import "AppDelegate.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface AppDelegate ()
 
-@property (weak) IBOutlet NSWindow *window;
+@property (nonatomic, readonly) NSStatusItem *statusItem;
+@property (nonatomic, readonly) AVPlayer *player;
 @end
 
 @implementation AppDelegate
 
+NSString * const kLastURL = @"last_url";
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    _statusItem.button.image = [NSImage imageNamed:@"StatusBarButtonImage"];
+
+    NSMenu *menu = [[NSMenu alloc] initWithTitle:@""];
+    [menu addItemWithTitle:@"Open" action:@selector(openNewUrl) keyEquivalent:@""];
+    [menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@""];
+    _statusItem.menu = menu;
+
+    _player = [AVPlayer playerWithPlayerItem:nil];
+    [self startPlayer:[[NSUserDefaults standardUserDefaults] URLForKey:kLastURL]];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    [_player pause];
+    [_player replaceCurrentItemWithPlayerItem:nil];
+}
+
+
+-(void)openNewUrl {
+    NSString *strurl = [AppDelegate getString:@"Stream URL to play:" :@""];
+    if ([strurl length] != 0) {
+        NSURL *url = [NSURL URLWithString:strurl];
+        [[NSUserDefaults standardUserDefaults] setURL:url forKey:kLastURL];
+        [self startPlayer:url];
+    }
+}
+
+
+-(void)startPlayer:(nullable NSURL *)url {
+    [_player pause];
+    if (url) {
+        [_player replaceCurrentItemWithPlayerItem:[AVPlayerItem playerItemWithURL:url]];
+        [_player setVolume:0.75];
+        [_player play];
+    } else {
+        [_player replaceCurrentItemWithPlayerItem:nil];
+    }
+}
+
+
++(NSString*)getString:(nonnull NSString *)message :(nonnull NSString *)defaultString {
+    NSTextField *input = [[NSTextField alloc] initWithFrame:NSMakeRect(0, 0, 400, 22)];
+    [input setStringValue:defaultString];
+    [input setMaximumNumberOfLines:1];
+
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:message];
+    [alert addButtonWithTitle:@"Ok"];
+    [alert addButtonWithTitle:@"Cancel"];
+    [alert setAccessoryView:input];
+    [[alert window] setInitialFirstResponder: input];
+
+    NSInteger button = [alert runModal];
+    return button == NSAlertFirstButtonReturn ? [input stringValue] : defaultString;
 }
 
 
