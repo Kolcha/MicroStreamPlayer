3dba1edb45bbf2bb769d03ca6cbe65ad50a40729
Nick Korotysh <kolchaprogrammer@list.ru>
2020-03-11 01:52:36 +0300

    Revert "restart playback in case of error"
    This is completely useless! Almost always when playback stops
    there are no any errors are reported. Even more, traffic doesn't
    stop, so it looks like player can't undarstand this traffic...
    
    This reverts commit cf190786178736273b1ebc19db9193cd636eefd1.

--- MicroStreamPlayer/AppDelegate.h
+++ MicroStreamPlayer/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <Cocoa/Cocoa.h>
 
-@interface AppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate>
+@interface AppDelegate : NSObject <NSApplicationDelegate>
 
 
 @end
