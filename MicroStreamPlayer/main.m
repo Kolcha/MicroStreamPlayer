68648a158a3ac6a87bad6ca6131b6aec8d5d2ec2
Nick Korotysh <nick.korotysh@gmail.com>
2025-12-23 12:07:03 +0100

    code modernization and improvements
    - proper property attributes
    - private class extension with mutable properties
    - validation and error handling
    - follow modern Objective-C patterns
    - minor UX improvements
    - minor code cleanup after restructurization

--- MicroStreamPlayer/main.m
+++ MicroStreamPlayer/main.m
@@ -10,54 +10,146 @@
 #import <AVFoundation/AVFoundation.h>
 
 @interface AppDelegate : NSObject <NSApplicationDelegate>
-@property (nonatomic, readonly) NSStatusItem *statusItem;
-@property (nonatomic, readonly) AVPlayer *player;
+@end
+
+@interface AppDelegate ()
+@property (nonatomic, strong) NSStatusItem *statusItem;
+@property (nonatomic, strong) AVPlayer *player;
 @end
 
 @implementation AppDelegate
 
-NSString *const kLastURL = @"last_url";
+static NSString *const kLastURL = @"last_url";
+static void *PlayerItemStatusContext = &PlayerItemStatusContext;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    _statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
-    _statusItem.button.image = [NSImage imageNamed:@"StatusBarButtonImage"];
+    self.statusItem = [[NSStatusBar systemStatusBar] statusItemWithLength:NSVariableStatusItemLength];
+    self.statusItem.button.image = [NSImage imageNamed:@"StatusBarButtonImage"];
 
     NSMenu *menu = [[NSMenu alloc] initWithTitle:@""];
     [menu addItemWithTitle:@"Open" action:@selector(openNewUrl) keyEquivalent:@""];
+    [menu addItem:[NSMenuItem separatorItem]];
     [menu addItemWithTitle:@"Quit" action:@selector(terminate:) keyEquivalent:@""];
-    _statusItem.menu = menu;
+    self.statusItem.menu = menu;
 
-    _player = [AVPlayer playerWithPlayerItem:nil];
+    self.player = [AVPlayer playerWithPlayerItem:nil];
     [self startPlayer:[[NSUserDefaults standardUserDefaults] URLForKey:kLastURL]];
 }
 
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    [_player pause];
-    [_player replaceCurrentItemWithPlayerItem:nil];
+    if (self.player.currentItem) {
+        [self.player.currentItem removeObserver:self forKeyPath:@"status" context:PlayerItemStatusContext];
+    }
+
+    [self.player pause];
+    [self.player replaceCurrentItemWithPlayerItem:nil];
 }
 
 
 - (void)openNewUrl {
-    NSString *strurl = [AppDelegate getString:@"Stream URL to play:" :@""];
-    if ([strurl length] != 0) {
-        NSURL *url = [NSURL URLWithString:strurl];
-        [[NSUserDefaults standardUserDefaults] setURL:url forKey:kLastURL];
-        [self startPlayer:url];
+    NSString *lastURLString = [[NSUserDefaults standardUserDefaults] URLForKey:kLastURL].absoluteString ?: @"";
+    NSString *strurl = [AppDelegate getString:@"Stream URL to play:" :lastURLString];
+
+    if ([strurl length] == 0) {
+        return;
     }
+
+    if ([strurl isEqualToString:lastURLString]) {
+        return;
+    }
+
+    NSURL *url = [NSURL URLWithString:strurl];
+    if (!url || (!url.scheme || (![url.scheme isEqualToString:@"http"] && ![url.scheme isEqualToString:@"https"]))) {
+        NSAlert *alert = [[NSAlert alloc] init];
+        [alert setMessageText:@"Invalid URL"];
+        [alert setInformativeText:@"Please enter a valid HTTP or HTTPS URL for the stream."];
+        [alert setAlertStyle:NSAlertStyleWarning];
+        [alert addButtonWithTitle:@"OK"];
+        [alert runModal];
+        return;
+    }
+
+    [[NSUserDefaults standardUserDefaults] setURL:url forKey:kLastURL];
+    [self startPlayer:url];
 }
 
 
 - (void)startPlayer:(nullable NSURL *)url {
-    [_player pause];
-    if (url) {
-        [_player replaceCurrentItemWithPlayerItem:[AVPlayerItem playerItemWithURL:url]];
-        [_player setVolume:0.75];
-        [_player play];
-    } else {
-        [_player replaceCurrentItemWithPlayerItem:nil];
+    [self.player pause];
+
+    // Remove observer from previous item if exists
+    if (self.player.currentItem) {
+        [self.player.currentItem removeObserver:self forKeyPath:@"status" context:PlayerItemStatusContext];
     }
-    [_statusItem.button setToolTip:url ? url.absoluteString : nil];
+
+    if (url) {
+        AVPlayerItem *playerItem = [AVPlayerItem playerItemWithURL:url];
+
+        // Add observer for player item status
+        [playerItem addObserver:self
+                     forKeyPath:@"status"
+                        options:NSKeyValueObservingOptionNew
+                        context:PlayerItemStatusContext];
+
+        // Register for failed notifications
+        [[NSNotificationCenter defaultCenter] addObserver:self
+                                                 selector:@selector(playerItemFailedToPlayToEnd:)
+                                                     name:AVPlayerItemFailedToPlayToEndTimeNotification
+                                                   object:playerItem];
+
+        [self.player replaceCurrentItemWithPlayerItem:playerItem];
+        [self.player setVolume:0.75];
+        [self.statusItem.button setToolTip:@"Loading..."];
+    } else {
+        [self.player replaceCurrentItemWithPlayerItem:nil];
+        [self.statusItem.button setToolTip:@"No stream loaded"];
+    }
+}
+
+
+- (void)observeValueForKeyPath:(NSString *)keyPath
+                      ofObject:(id)object
+                        change:(NSDictionary *)change
+                       context:(void *)context {
+    if (context == PlayerItemStatusContext) {
+        AVPlayerItem *playerItem = (AVPlayerItem *)object;
+
+        if ([keyPath isEqualToString:@"status"]) {
+            switch (playerItem.status) {
+                case AVPlayerItemStatusReadyToPlay:
+                    NSLog(@"Player ready to play");
+                    [self.player play];
+                    [self.statusItem.button setToolTip:((AVURLAsset *)playerItem.asset).URL.absoluteString];
+                    break;
+
+                case AVPlayerItemStatusFailed:
+                    [self handlePlayerError:playerItem.error];
+                    break;
+
+                case AVPlayerItemStatusUnknown:
+                    NSLog(@"Player status unknown");
+                    break;
+            }
+        }
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+
+- (void)handlePlayerError:(nullable NSError *)error {
+    NSString *errorMessage = error ? error.localizedDescription : @"Unknown error occurred";
+    NSLog(@"Player error: %@", errorMessage);
+
+    [self.player pause];
+    [self.statusItem.button setToolTip:@"Error: Failed to play stream"];
+}
+
+
+- (void)playerItemFailedToPlayToEnd:(NSNotification *)notification {
+    NSError *error = notification.userInfo[AVPlayerItemFailedToPlayToEndTimeErrorKey];
+    [self handlePlayerError:error];
 }
 
 
